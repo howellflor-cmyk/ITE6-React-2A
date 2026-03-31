d2a3d31c1117191a2db88b53243f7e3d862aaccf
Howell <howellflor@gmail.com>
2026-03-31 01:24:44 -0400

    first commit

---
+++ Group-4/src/Landing Page/Home.jsx
@@ -0,0 +1,20 @@
+import "./Home.css";
+
+const Home = () => {
+  return (
+    <main className="container">
+      <h1>2A Group 7 Members</h1>
+      <p>This is our group landing page with four dedicated members.</p>
+      <section>
+                <ul>
+                        <li><strong>Member 1:</strong> Jhon Keith Lhexter Bonayog</li>
+                        <li><strong>Member 2:</strong> Yancy Jess Martinez</li>
+                        <li><strong>Member 3:</strong> Rodnie Banacia</li>
+                        <li><strong>Member 4:</strong> Howell Labradores</li> 
+                </ul>
+      </section>
+    </main>
+  );
+}
+
+export default Home;
