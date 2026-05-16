1c0b02be1694f51b3c809145fa9066a858d48868
Howell <howellflor@gmail.com>
2026-05-16 04:46:38 -0400

    fix App.jsx imports

--- Group-4/src/App.jsx
+++ Group-4/src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom"
-import MCO from "./pages/mco/mco"
+import MCO from "./pages/mco/MCO"
 import Activity1 from "./pages/activity1/Home"
 import Activity2 from "./pages/activity2/Moodswing"
 import Activity3 from "./pages/activity3/Activity3"
