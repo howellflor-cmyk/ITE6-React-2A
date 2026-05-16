e4574c2cf4d1a139f7fdb3939445fb5caf6e338f
Howell <howellflor@gmail.com>
2026-05-16 04:49:39 -0400

    fix Pages capital P

--- Group-4/src/App.jsx
+++ Group-4/src/App.jsx
@@ -1,9 +1,9 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom"
-import MCO from "./pages/mco/MCO"
-import Activity1 from "./pages/activity1/Home"
-import Activity2 from "./pages/activity2/Moodswing"
-import Activity3 from "./pages/activity3/Activity3"
-import Activity4 from "./pages/activity4/Titan"
+import MCO from "./Pages/MCO/MCO"
+import Activity1 from "./Pages/activity1/Home"
+import Activity2 from "./Pages/activity2/Moodswing"
+import Activity3 from "./Pages/activity3/Activity3"
+import Activity4 from "./Pages/Activity4/Titan"
 
 const App = () => {
   return (
